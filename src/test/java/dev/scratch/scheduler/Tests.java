9f295b9d3a0cd0d9ad3fefdc4af032534cca3693
Jiaming <Jiamingw29@gmail.com>
2023-06-16 18:39:35 -0400

    updated tests

--- src/test/java/dev/scratch/scheduler/Tests.java
+++ src/test/java/dev/scratch/scheduler/Tests.java
@@ -20,18 +20,20 @@ public class Tests {
         TimeFrame timeFrame1 = new TimeFrame(localTime1, localTime2);
         TimeFrame timeFrame2 = new TimeFrame(localTime3, localTime4);
         TimeFrame timeFrame3 = new TimeFrame(LocalTime.MIDNIGHT, LocalTime.of(10, 0, 0));
-        TimeFrame timeFrame4 = new TimeFrame(LocalTime.of(14, 0, 0), LocalTime.of(23, 59, 59));
+        TimeFrame timeFrame4 = new TimeFrame(LocalTime.of(15, 0, 0), LocalTime.of(23, 59, 59));
 
         ScheduleService scheduleService = new ScheduleService();
-        HardAction hardAction1 = new HardAction("Lunch", timeFrame2, new DayOfWeek[]{DayOfWeek.MONDAY},0,0);
-        HardAction hardAction2 = new HardAction("Morning Class", timeFrame1, new DayOfWeek[]{DayOfWeek.MONDAY},0,0);
+        HardAction hardAction1 = new HardAction("Lunch", timeFrame2, new DayOfWeek[]{DayOfWeek.MONDAY}, 0, 0);
+        HardAction hardAction2 = new HardAction("Morning Class", timeFrame1, new DayOfWeek[]{DayOfWeek.MONDAY}, 0, 0);
         HardAction hardAction3 = new HardAction("Sleep", timeFrame3, new DayOfWeek[]{DayOfWeek.MONDAY});
         Constraint constraint2 = new Constraint("Break", timeFrame4, new DayOfWeek[]{DayOfWeek.MONDAY});
         scheduleService.addAction(hardAction1);
         scheduleService.addAction(hardAction2);
         scheduleService.addAction(hardAction3);
         scheduleService.addConstraint(constraint2);
-        scheduleService.addAction(new SoftAction(Duration.ofMinutes(30), "Task 1", new DayOfWeek[]{DayOfWeek.MONDAY},0,0));
+        scheduleService.addAction(new SoftAction(Duration.ofMinutes(30), "Task 1", new DayOfWeek[]{DayOfWeek.MONDAY}, 0, 0));
+        scheduleService.addAction(new SoftAction(Duration.ofMinutes(45), "Task 2", new DayOfWeek[]{DayOfWeek.MONDAY}, 0, 30));
+        scheduleService.addAction(new SoftAction(Duration.ofMinutes(45), "Task 3", new DayOfWeek[]{DayOfWeek.MONDAY}, 15, 30));
         scheduleService.schedule();
         scheduleService.getSchedule(DayOfWeek.MONDAY).printSchedule();
 
